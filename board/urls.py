26e83923fe94ec955a80ddaf86eabc7640b3a568
swwon-i <stonesw123@naver.com>
2025-06-17 12:33:52 +0900

    tlqkf

--- board/urls.py
+++ board/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
 
+# 네임스페이스
+## board:question_list
+
+app_name = 'board'
+
 urlpatterns = [
-    path('', views.index),  
-    path('<int:question_id>/', views.detail)
+    path('', views.index, name='question_list'),  
+    path('<int:question_id>/', views.detail, name='question_detail')
 ]
--- board/views.py
+++ board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question, Answer
 
@@ -7,6 +7,9 @@ def index(request):
     return render(request, 'board/index.html', {'questions': questions})
 
 def detail(request, question_id):
-    question = Question.objects.get(id=question_id)
+    # question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, id=question_id)
     answers = Answer.objects.filter(question=question).order_by('-created_at')
+    # answers = get_object_or_404(Answer, id=question_id)
+            
     return render(request, 'board/detail.html', {'question' : question, 'answer':answers})
